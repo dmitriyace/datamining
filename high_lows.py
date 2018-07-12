7ddb03e79ea06233d8da81c49fb45a804233d4f9
dmitriyace <chistohodovda@gmail.com>
2018-07-13 00:51:34 +0300

    fill_between

--- high_lows.py
+++ high_lows.py
@@ -3,8 +3,8 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 # reading max_temerature and dates from csv
-filename = "../sitka_weather_07-2014.csv"
-dates, highs = [], []
+filename = "sitka_weather_07-2014.csv"
+dates, highs, lows = [], [], []
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -14,14 +14,16 @@ with open(filename) as f:
         dates.append(current_date)
         high = int(row[1])
         highs.append(high)
-
-# print(highs)
+        low = int(row[3])
+        lows.append(low)
 
 # putting data on the diagram
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue',alpha=0.1)
 # diagram formatting
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high 'n low temperatures, July 2014", fontsize=24)
 plt.xlabel('', fontsize=12)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=12)
